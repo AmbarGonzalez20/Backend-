18a26b416db1cc5055ef53c1c55048c7914591d4
Ambar <2230016@upt.edu.mx>
2026-08-09 15:06:18 -0600

    Actualiza base de datos

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -10,7 +10,7 @@ object BachesTable : IntIdTable("baches") {
     val latitud = double("latitud")
     val longitud = double("longitud")
     val fotoUrl = varchar("foto_url", 500).default("")
-    val fechaReporte = varchar("fecha_reporte", 100).default("")
+
     val estado = varchar("estado", 50).default("pendiente")
 
     val usuarioId = integer("usuario_id").default(0)
